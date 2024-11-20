3da35da89ab857caaf414862c7aff0a091c3c929
Omer Gillani <sogillani@gmail.com>
2024-11-20 16:56:39 +0100

    add h2 database for tests
    - defined event table schema

--- src/test/java/com/blxtech/joinpromise/service/ServiceApplicationTests.java
+++ src/test/java/com/blxtech/joinpromise/service/ServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.blxtech.joinpromise.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = "test")
 class ServiceApplicationTests {
 
 	@Test
